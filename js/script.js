8dbe22dd77bcfe3a9eea1faf2b99a1f04cb75741
Lelalorim <Liwonec@gmail.com>
2021-05-27 14:23:26 +0300

    add html and script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log(1213);
+let money = 10000;
+let income = 30000;
+let addExpenses = 5000;
+let deposit = 12000;
+let mission = 500000;
+let period = 6;
+
+alert('Сообщение в модальном окне "alert"');
+
+console.log('Нужно накопить за ' + period + ' месяцев, сумму равной ' + mission + ' рублей.');
