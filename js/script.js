6e48816d0854dea91e0d9180fb82e2f57d456c87
Lelalorim <Liwonec@gmail.com>
2021-05-28 09:16:23 +0300

    chore(script.js): Исправил ДЗ№1 по правкам куратора

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-let money = 10000;
-let income = 30000;
-let addExpenses = 5000;
-let deposit = 12000;
-let mission = 500000;
-let period = 6;
+'use strict';
+
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
 alert('Сообщение в модальном окне "alert"');
 
-console.log('Нужно накопить за ' + period +
-  ' месяцев, сумму равной ' + mission + ' рублей.');
+console.log('Обсудить типы коммитов: "chore", "style", "refactor".');
