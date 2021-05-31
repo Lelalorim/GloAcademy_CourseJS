9a472759c3f50dcf8d4feed2949786b9005cabca
Lelalorim <Liwonec@gmail.com>
2021-05-31 13:10:01 +0300

    refactor(script.js): made edits from the curator

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
-let money = 42000;
-let income = 'Фриланс';
-let addExpenses = 'Интернет, Коммуналка, Мобильный';
-let deposit = true;
-let mission = 500000;
-let period = 9;
+let money = 42000,
+  income = 'Фриланс',
+  addExpenses = 'Интернет, Коммуналка, Мобильный',
+  deposit = true,
+  mission = 500000,
+  period = 9,
+  budgetDay = (money / 30);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -17,12 +18,5 @@ console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей.');
 
 console.log(addExpenses.toLocaleLowerCase().split(', '));
-
-/* через дополнительную переменную
-let addExpensesLowerCase = addExpenses.toLocaleLowerCase();
-console.log(addExpensesLowerCase.split(', '));
-*/
-
-let budgetDay = (money / 30);
 
 console.log(budgetDay);
