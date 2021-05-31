60c84c94f0e30dec07f1da35671b9f9cd74e67fc
Lelalorim <Liwonec@gmail.com>
2021-05-31 17:22:43 +0300

    feat(script.js): hard HW03 done

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 'use strict';
 
-let num = 266219;
+let lang = prompt('Выберите язык: "en" или "ru"', 'ru'),
+  kalendar = [
+    ['Пн', ' Вт', ' Ср', ' Чт', ' Пт', ' Сб', ' Вс'],
+    ['Sun', ' Mon', ' Tue', ' Wed', ' Thu', ' Fri', ' Sat']
+  ],
+  namePerson;
 
-// Перебор цифр из числа по порядку
-function multiplySum(arr) {
-  let sum = 1;
-  let elem = String(arr); //перевод в строчный тип данных
-  //console.log(typeof (elem), elem);
-
-  for (let i = 0; i < elem.length; i++) {
-    sum *= Number(elem[i]); //умножение на следующий элемент. Number - перевод в числовой тип данных.
-    //console.log(elem[i]);
-  }
-  return (sum);
+if (lang === 'ru') {
+  console.log('a.: Пн, Вт, Ср, Чт, Пт, Сб, Вс');
+} else if (lang === 'en') {
+  console.log('a.: Sun, Mon, Tue, Wed, Thu, Fri, Sat');
+} else {
+  alert('Нужно ввести "en" или "ru"');
+  console.log('Нужно ввести "en" или "ru"');
 }
 
-console.log(multiplySum(num));
+switch (lang) {
+  case 'ru':
+    console.log('b.: Пн, Вт, Ср, Чт, Пт, Сб, Вс');
+    break;
+  case 'en':
+    console.log('b.: Sun, Mon, Tue, Wed, Thu, Fri, Sat');
+    break;
+}
 
-let stepen = multiplySum(num) ** 3;
-//console.log('stepen: ', stepen);
+lang === 'ru' ? console.log('c.: ' + kalendar[0]) : console.log('c.: ' + kalendar[1]);
 
-console.log(String(stepen).substr(0, 2));
-//console.log(String(stepen).substring(0, 2));
+
+namePerson = prompt('Введите имя: ')
+namePerson === 'Артем' ? console.log('директор') :
+  namePerson === 'Максим' ? console.log('преподаватель') :
+  console.log('студент');
