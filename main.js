1fc1034d7a6ce609e45654826c99ddc60a445679
Lelalorim <Liwonec@gmail.com>
2021-06-01 12:03:27 +0300

    refactor(main.js): use Array.map

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 'use strict';
 
 let lang = prompt('Выберите язык: "en" или "ru"', 'ru'),
-  kalendar = [
-    ['Пн', ' Вт', ' Ср', ' Чт', ' Пт', ' Сб', ' Вс'],
-    ['Sun', ' Mon', ' Tue', ' Wed', ' Thu', ' Fri', ' Sat']
-  ],
   kalendarMap = new Map(),
   namePerson;
 
-kalendarMap.set('en', kalendar[1]);
-kalendarMap.set('ru', kalendar[0]);
+kalendarMap.set('ru', ['Пн', ' Вт', ' Ср', ' Чт', ' Пт', ' Сб', ' Вс']);
+kalendarMap.set('en', ['Sun', ' Mon', ' Tue', ' Wed', ' Thu', ' Fri', ' Sat']);
 
 if (lang === 'ru') {
-  console.log(`a.: ${kalendar[0]}`);
+  console.log(`a.: ${kalendarMap.get(lang)}`);
 } else if (lang === 'en') {
-  console.log(`a.: ${kalendar[1]}`);
+  console.log(`a.: ${kalendarMap.get(lang)}`);
 } else {
   alert('Нужно ввести "en" или "ru"');
   console.log('Нужно ввести "en" или "ru"');
@@ -22,14 +18,14 @@ if (lang === 'ru') {
 
 switch (lang) {
   case 'ru':
-    console.log(`b.: ${kalendar[0]}`);
+    console.log(`b.: ${kalendarMap.get(lang)}`);
     break;
   case 'en':
-    console.log(`b.: ${kalendar[1]}`);
+    console.log(`b.: ${kalendarMap.get(lang)}`);
     break;
 }
 
-console.log('c.: ' + kalendarMap.get(`${lang}`));
+console.log(`c.: ${kalendarMap.get(lang)}`);
 
 namePerson = prompt('Введите имя: ');
 namePerson === 'Артем' ? console.log('директор') :
