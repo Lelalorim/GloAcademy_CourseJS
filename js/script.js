0b45bb2a107693452eb84e8998bd007b0daf42b5
Lelalorim <Liwonec@gmail.com>
2021-06-01 12:29:04 +0300

    refactor(script.js): Внес правки, используя шаблонные строки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-let money = +prompt('Укажите ваш месячный доход'),
+const money = +prompt('Укажите ваш месячный доход'),
   income = 'Фриланс',
   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Коммуналка, Мобильный'),
   deposit = confirm('Есть ли у вас депозит в банке?'),
   expensesOne = prompt('Введите обязательную статью расходов?', addExpenses.split(', ')[0]),
-  amountOne = +prompt('Сколько вы тратите на оплату ' + expensesOne + ' ?'),
+  amountOne = +prompt(`Сколько вы тратите на оплату ${expensesOne}?`),
   expensesTwo = prompt('Введите обязательную статью расходов?', addExpenses.split(', ')[1]),
-  amountTwo = +prompt('Сколько вы тратите на оплату ' + expensesTwo + ' ?'),
+  amountTwo = +prompt(`Сколько вы тратите на оплату ${expensesTwo}?`),
   budgetMonth = money - (amountOne + amountTwo),
   mission = 500000,
   period = 9,
@@ -17,12 +17,12 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей.');
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей.`);
 console.log(addExpenses.toLocaleLowerCase().split(', '));
-console.log('Бюджет на месяц ' + budgetMonth);
-console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев(-a)');
-console.log('Бюджет на день ' + Math.floor(budgetDay));
+console.log(`Бюджет на месяц ${budgetMonth}`);
+console.log(`Цель будет достигнута за ${Math.ceil(mission / budgetMonth)} месяцев(-a)`);
+console.log(`Бюджет на день ${Math.floor(budgetDay)}`);
 
 if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
