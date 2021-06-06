1f35ea2b2e5ca9c57e4900037c9de573753e19bd
Lelalorim <liwonec@gmail.com>
2021-06-06 11:37:56 +0300

    refactor(main.js): упростил код

--- main.js
+++ main.js
@@ -2,31 +2,19 @@
 
 function shortDescript(stroka) {
 
-  function cutSpaces(arg) {
-    if (arg === null) {
-      return '';
-    } else {
-      return arg.trim();
-    }
-  }
-
-  function shortString(par) {
-    if (cutSpaces(par).length < 30) {
-      return cutSpaces(par);
-    } else {
-      return (`${cutSpaces(par).substring(0, 30)}...`);
-    }
-  }
-
   const isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
 
-  while (typeof (stroka) !== 'string' || cutSpaces(stroka).length === 0 || isNumber(stroka)) {
+  while (typeof (stroka) !== 'string' || stroka.trim().length === 0 || isNumber(stroka)) {
     stroka = prompt('введите строку', '');
   }
 
-  return console.log('Краткое описание:', shortString(stroka));
+  let shortString = stroka.trim();
+
+  stroka = (shortString.length < 30) ? shortString : (`${shortString.substring(0, 30)}...`);
+
+  return console.log('Краткое описание:' + stroka);
 }
 
 shortDescript();
