fcd937982d9a1fef48fcc3de59097c8227fbdcef
Lelalorim <Liwonec@gmail.com>
2021-06-09 13:08:07 +0300

    style(main.js): change else..if, add const for functions, edit (count--; / checkAnswer(--count))

--- main.js
+++ main.js
@@ -3,25 +3,26 @@
 function letsPlay() {
 
   let count = 10;
-  const isNumber = function (n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    },
-    gameOver = function () {
-      alert('Игра окончена.');
-    },
-    letAnotherGame = function () {
-      confirm('Попытки закончились, хотите сыграть еще?') ?
-        letsPlay() :
-        gameOver();
-    },
-    gameWin = function () {
-      confirm(`Поздравляю, Вы угадали!!! Осталось попыток ${count}
-      Хотели бы сыграть еще?`) ?
-        letsPlay() :
-        gameOver();
-    },
-    countMax = count + 1,
+  const countMax = count + 1,
     getRandomNumber = Math.floor(Math.random() * 100) + 1;
+
+  const isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+  const gameOver = function () {
+    alert('Игра окончена.');
+  };
+  const letAnotherGame = function () {
+    confirm('Попытки закончились, хотите сыграть еще?') ?
+      letsPlay() :
+      gameOver();
+  };
+  const gameWin = function () {
+    confirm(`Поздравляю, Вы угадали!!! Осталось попыток ${count}
+      Хотели бы сыграть еще?`) ?
+      letsPlay() :
+      gameOver();
+  };
 
   function getAnswer(ans) {
     if (count === 0) {
@@ -37,19 +38,22 @@ function letsPlay() {
 
       if (ans === getRandomNumber) {
         gameWin();
-      } else if (ans === 0) {
+      }
+      if (ans === 0) {
         gameOver();
-      } else if (ans < getRandomNumber) {
+        return;
+      }
+      if (ans < getRandomNumber) {
         alert(`Загаданное число больше, осталось попыток ${count}.`);
         getAnswer();
-      } else {
+      }
+      if (ans > getRandomNumber) {
         alert(`Загаданное число меньше, осталось попыток ${count}.`);
         getAnswer();
       }
     }
 
-    count--;
-    return checkAnswer(count);
+    return checkAnswer(--count);
   }
 
   confirm('Угадай число от 1 до 100.') ? getAnswer() : gameOver();
