7f7b6431d813cb5673be04b870f8d38c709bf93e
Lelalorim <Liwonec@gmail.com>
2021-07-06 17:02:33 +0300

    feat(script.js): add animation for menu and open popup

--- script/script.js
+++ script/script.js
@@ -42,5 +42,61 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 1000);
 	}
 
-	countTimer('05 july 2021 14:27');
+	countTimer('15 july 2021 14:27');
+
+	// Menu
+	const toogleMenu = () => {
+
+		const btnMenu = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			closeBtn = document.querySelector('.close-btn'),
+			menuItems = menu.querySelectorAll('ul>li');
+
+		const handlerMenu = () => menu.classList.toggle('active-menu');
+
+		btnMenu.addEventListener('click', handlerMenu);
+		closeBtn.addEventListener('click', handlerMenu);
+		menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+	};
+
+	toogleMenu();
+
+	// popup
+	const togglePopup = () => {
+		const popup = document.querySelector('.popup'),
+			popupClose = document.querySelector('.popup-close'),
+			popupBtn = document.querySelectorAll('.popup-btn');
+
+		popupBtn.forEach(elem => elem.addEventListener('click', () => {
+			popup.style.display = 'block';
+
+			//	if (screen > 768) {
+			let stepLeft = -110;
+			const popupBlock = document.querySelector('.popup-content');
+
+			function popupOpen() {
+				if (stepLeft < 38) {
+					stepLeft += 5;
+					popupBlock.style.left = stepLeft + "%";
+					requestAnimationFrame(popupOpen);
+				}
+			}
+			requestAnimationFrame(popupOpen);
+			//	}
+
+
+		}));
+
+		popupClose.addEventListener('click', () => {
+			popup.style.display = 'none';
+		});
+
+		// popupOpen();
+
+
+	};
+
+	togglePopup();
+
 });
