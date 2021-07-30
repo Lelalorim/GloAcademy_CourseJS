86ecd62d057970e3641eb6dc14d21f5440ec97bc
Lelalorim <Liwonec@gmail.com>
2021-07-30 11:12:55 +0300

    refactor(script.js): refactor send form with use fetch()

--- script/script.js
+++ script/script.js
@@ -445,28 +445,20 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 			target.reset();
 
-			const postData = body => new Promise((resolve, reject) => {
+			const postData = body => {
+				return fetch('./server.php', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(body),
+				})
 
-				const request = new XMLHttpRequest();
-				request.addEventListener('readystatechange', () => {
-					if (request.readyState !== 4) {
-						return;
-					}
-					if (request.status === 200) {
-						resolve();
-					} else {
-						reject(request.status);
-					}
-
-				});
-
-				request.open('POST', './server.php');
-				request.setRequestHeader('Content-Type', 'application/json');
-				request.send(JSON.stringify(body));
-			});
+			};
 
 			postData(body)
-				.then(() => {
+				.then((response) => {
+					if (response.status !== 200) throw new Error('status network not 200');
 					statusMessage.textContent = successMessage;
 				})
 				.catch(error => {
